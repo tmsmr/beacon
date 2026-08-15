d97ad16518fa3aee50b2296bf0c5bb075a308f00
Thomas Maier <contact@thomas-maier.net>
2026-08-15 16:32:56 +0200

    adds tiny usb config (hid keyboard descriptor)

--- firmware/beacon.cpp
+++ firmware/beacon.cpp
@@ -1,5 +1,6 @@
 #include "pico/stdlib.h"
 #include <sk6812.h>
+#include "tusb.h"
 
 #define SK6812_PIN 0
 
@@ -10,6 +11,20 @@
     led.setPixelColor(0, 255, 0, 0, 0);
     led.show();
 
+    tusb_init();
+
     while (true) {
+        tud_task();
     }
+}
+
+uint16_t tud_hid_get_report_cb(
+    uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t *buffer, uint16_t reqlen
+) {
+    return 0;
+}
+
+void tud_hid_set_report_cb(
+    uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const *buffer, uint16_t bufsize
+) {
 }
---
+++ firmware/usb_descriptors.c
@@ -0,0 +1,136 @@
+#include "tusb.h"
+#include "bsp/board_api.h"
+
+#define USB_BCD 0x0200 // USB 2.0
+
+#define BEACON_USB_VID 0x1209
+#define BEACON_USB_PID 0x0001 // TODO: get one at https://pid.codes/
+#define BEACON_USB_DEVICE_VER 0x0010
+
+#define BEACON_MANUFACTURER "tmsmr"
+#define BEACON_PRODUCT_NAME "beacon"
+
+#define REPORT_ID_KEYBOARD 1
+
+#define POWER_CONSUMPTION_MA 100 // TODO: measure
+
+#define DEVICE_USB_POLL_FRAMES  5
+
+//
+// DEVICE DESCRIPTOR
+//
+
+tusb_desc_device_t const desc_device = {
+    .bLength = sizeof(tusb_desc_device_t),
+    .bDescriptorType = TUSB_DESC_DEVICE,
+    .bcdUSB = USB_BCD,
+    .bDeviceClass = 0x00,
+    .bDeviceSubClass = 0x00,
+    .bDeviceProtocol = 0x00,
+    .bMaxPacketSize0 = CFG_TUD_ENDPOINT0_SIZE,
+
+    .idVendor = BEACON_USB_VID,
+    .idProduct = BEACON_USB_PID,
+    .bcdDevice = BEACON_USB_DEVICE_VER,
+
+    .iManufacturer = 0x01,
+    .iProduct = 0x02,
+    .iSerialNumber = 0x03,
+
+    .bNumConfigurations = 0x01
+};
+
+uint8_t const *tud_descriptor_device_cb(void) {
+    return (uint8_t const *) &desc_device;
+}
+
+//
+// HID REPORT DESCRIPTOR
+//
+
+uint8_t const desc_hid_report[] = {
+    TUD_HID_REPORT_DESC_KEYBOARD(HID_REPORT_ID(REPORT_ID_KEYBOARD))
+};
+
+uint8_t const *tud_hid_descriptor_report_cb(uint8_t instance) {
+    (void) instance;
+    return desc_hid_report;
+}
+
+// CONFIGURATION DESCRIPTOR
+
+enum {
+    ITF_NUM_HID,
+    ITF_NUM_TOTAL
+};
+
+#define CONFIG_TOTAL_LEN (TUD_CONFIG_DESC_LEN + TUD_HID_DESC_LEN)
+
+#define HID_EP_NUM 0x81 // keyboard
+
+uint8_t const desc_configuration[] = {
+    TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, 0, POWER_CONSUMPTION_MA),
+    TUD_HID_DESCRIPTOR(
+        ITF_NUM_HID, 0, HID_ITF_PROTOCOL_NONE, sizeof(desc_hid_report),
+        HID_EP_NUM, CFG_TUD_HID_EP_BUFSIZE, DEVICE_USB_POLL_FRAMES
+    )
+};
+
+uint8_t const *tud_descriptor_configuration_cb(uint8_t index) {
+    (void) index;
+    return desc_configuration;
+}
+
+//
+// STRING DESCRIPTOR
+//
+
+enum {
+    STRID_LANGID = 0,
+    STRID_MANUFACTURER,
+    STRID_PRODUCT,
+    STRID_SERIAL,
+};
+
+static char const *string_desc_arr[] = {
+    (const char[]){0x09, 0x04}, // en_US
+    BEACON_MANUFACTURER,
+    BEACON_PRODUCT_NAME,
+    NULL,
+};
+
+static uint16_t _desc_str[32 + 1];
+
+uint16_t const *tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
+    (void) langid;
+    size_t chr_count;
+
+    switch (index) {
+        case STRID_LANGID:
+            memcpy(&_desc_str[1], string_desc_arr[0], 2);
+            chr_count = 1;
+            break;
+
+        case STRID_SERIAL:
+            chr_count = board_usb_get_serial(_desc_str + 1, 32);
+            break;
+
+        default:
+            if (index >= sizeof(string_desc_arr) / sizeof(string_desc_arr[0])) return NULL;
+
+            const char *str = string_desc_arr[index];
+
+            chr_count = strlen(str);
+            size_t const max_count = sizeof(_desc_str) / sizeof(_desc_str[0]) - 1;
+            if (chr_count > max_count) chr_count = max_count;
+
+            for (size_t i = 0; i < chr_count; i++) {
+                _desc_str[1 + i] = str[i];
+            }
+            break;
+    }
+
+    _desc_str[0] = (uint16_t) ((TUSB_DESC_STRING << 8) | (2 * chr_count + 2));
+
+    return _desc_str;
+}
